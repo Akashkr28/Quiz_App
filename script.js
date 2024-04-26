1ac7b7aeaac2522dd95728a167c416f5f03caa14
Akashkr28 <email.akash2010@gmail.com>
2024-04-27 04:28:28 +0530

    quiz app

--- script.js
+++ script.js
@@ -49,3 +49,26 @@ const questions = [
 const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answer_buttons");
 const nextButton = document.getElementById("next_btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    let currentQuestion = question[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answer.array.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
+}
